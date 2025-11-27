842dcee9201f7aad1f125e7cb8b0afde73895a9d
Victoria Belo <vickbelo17@gmail.com>
2025-11-27 17:14:03 -0300

    LH-12: DTO class, contructor, methods, validations

--- src/main/java/com/project/library/DTO/UserDTO.java
+++ src/main/java/com/project/library/DTO/UserDTO.java
@@ -1,7 +1,6 @@
 package com.project.library.DTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class UserDTO {
