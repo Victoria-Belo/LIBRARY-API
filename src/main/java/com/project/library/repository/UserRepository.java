5fcee211fb9fed5d61df2358ec7d280463286125
Victoria Belo <vickbelo17@gmail.com>
2025-11-27 16:29:42 -0300

    LH-10: added existsByEmail()

--- src/main/java/com/project/library/repository/UserRepository.java
+++ src/main/java/com/project/library/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findById(long id);
-    Boolean findByEmail(String email);
+    boolean existsByEmail(String email);
 }
