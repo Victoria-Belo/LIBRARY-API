95fdec92d8ff2f2e511dcaa590a65a6cf89e60c5
Victoria Belo <vickbelo17@gmail.com>
2025-11-27 16:23:53 -0300

    LH-14 added patch routes

--- src/main/java/com/project/library/controller/UserController.java
+++ src/main/java/com/project/library/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.project.library.controller;
+import com.project.library.DTO.UserDTO;
 import com.project.library.model.User;
 import com.project.library.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/user")
 public class UserController {
 
     private final UserService userService;
@@ -15,24 +17,34 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public User getUser(@PathVariable Long id){
         return userService.user(id);
     }
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public List<User> getUsers(){
         return userService.users();
     }
 
-    @DeleteMapping("user/{id}")
+    @PostMapping("/")
+    public User createUser(@RequestBody UserDTO dto) {
+        return userService.createUser(dto);
+    }
+
+    @DeleteMapping("/{id}")
     public User removeUser(@PathVariable Long id){
         userService.remove(id);
         return null;
     }
 
-    @PutMapping("user/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User user){
-        return userService.update(id, user);
+    @PatchMapping("email/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody UserDTO email){
+        return userService.update(id, email);
+    }
+
+    @PatchMapping("password/{id}")
+    public User updatePassword(@PathVariable Long id, @RequestBody UserDTO password){
+        return userService.updatePassword(id, password);
     }
 }
