d68c5850ee97683eef9bd610d4dfd6f970ac8e1d
Victoria Belo <vickbelo17@gmail.com>
2025-11-27 16:35:17 -0300

    added validations and constructor

--- src/main/java/com/project/library/DTO/UserDTO.java
+++ src/main/java/com/project/library/DTO/UserDTO.java
@@ -1,7 +1,45 @@
 package com.project.library.DTO;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 5, message = "A senha deve ter pelo menos 5 caracteres")
     private String password;
+
+    public UserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
