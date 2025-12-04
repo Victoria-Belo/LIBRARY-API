42007441dbbcfd1898ebaeb37c3e9d3dd0096cd5
Victoria Belo <vickbelo17@gmail.com>
2025-12-04 00:29:43 -0300

    LH-16: feat LoginResponseDTO

---
+++ src/main/java/com/project/library/DTO/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.project.library.DTO;
+
+public class LoginResponseDTO {
+
+    private String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
